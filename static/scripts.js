f471ed5a99832884b210b8a7a1bf91fde15728a7
CallumCarter <callum_carter@live.com>
2020-03-10 13:23:03 +0000

    Removed DS script

--- static/scripts.js
+++ static/scripts.js
@@ -1,21 +1,3 @@
-function profileUndisable(field) {
-    var input = document.getElementById(field);
-    var button = document.getElementById("button-" + field)
-    var isDisabled = input.hasAttribute("disabled");
-    var form = document.getElementById("update-" + field);
-
-    // if the form is disabled enable it
-    if (isDisabled) {
-        input.toggleAttribute('disabled');
-        button.classList.remove('btn-outline-secondary');
-        button.classList.add('btn-success');
-        button.innerHTML = "Submit Change";
-    } else {
-        form.submit();
-    }
-};
-
-
 $(document).ready(function () {
     ('[data-toggle="tooltip"]').tooltip();
 });
